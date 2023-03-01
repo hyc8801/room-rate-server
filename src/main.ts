0ef8e684615722e50603107a3fe2bea26a5509d2
hyc <yongchuan.huang@socialcredits.cn>
2023-03-01 10:02:50 +0800

    🚀 build: 修改端口号

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3100);
+  await app.listen(3200);
 }
 bootstrap();
